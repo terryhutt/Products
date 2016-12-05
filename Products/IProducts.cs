b855d1d3b7e49a673dff70f10ecbda502f4a9ca9
terry.hutt <terry.hutt@IT86174>
2016-12-05 09:58:37 -0800

    Add project files.

---
+++ Products/IProducts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Products
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string ProductNumber { get; set; }
+
+        [DataMember]
+        public string Color { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IProducts
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "ProductList/")]
+        List<Product> GetProductList();
+    }
+}
---
+++ Products/Products.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Products
+{
+    public class Products : IProducts
+    {
+        public List<Product> GetProductList() 
+        {
+            String SQL = "SELECT ProductID, Name, ProductNumber, Color, ListPrice FROM Production.Product ORDER BY ProductID";
+            String ConnString = System.Configuration.ConfigurationManager.ConnectionStrings["localhost"].ConnectionString;
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                conn.Open();
+                using (SqlCommand comm = new SqlCommand(SQL, conn))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(comm))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+            List<Product> ProductList = dt.AsEnumerable().Select(r => new Product()
+            {
+                ID = r.Field<int>("ProductID"),
+                Name = r.Field<String>("Name"),
+                ProductNumber = r.Field<String>("ProductNumber"),
+                Color = r.Field<String>("Color"),
+                Price = r.Field<Decimal>("ListPrice")
+            }).ToList();
+            return ProductList;
+        }
+
+    }
+}
